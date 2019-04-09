231213465f7b1e6ecc1f5a7f1c4acbe1a8645a36
Danolo AlcNom <danoloan10@gmail.com>
2019-04-09 22:12:58 +0200

    Añadida documentación

--- nunit.h
+++ nunit.h
@@ -3,18 +3,79 @@
 
 #include <stddef.h>
 
+/* This function compares the two integers <<actual>> and <<expected>>
+ * 	If they are equal, the function returns 0
+ * 	If they are not equal, the formatted message <<msh, ...>>
+ * 		is printed and -1 is returned
+ */
 int assert_equals_int(int actual, int expected, const char * msg, ...);
+/* This function compares the two characters <<actual>> and <<expected>>
+ * 	If they are equal, the function returns 0
+ * 	If they are not equal, the formatted message <<msh, ...>>
+ * 		is printed and -1 is returned
+ */
 int assert_equals_char(char actual, char expected, const char * msg, ...);
+/* This function compares the two short <<actual>> and <<expected>>
+ * 	If they are equal, the function returns 0
+ * 	If they are not equal, the formatted message <<msh, ...>>
+ * 		is printed and -1 is returned
+ */
 int assert_equals_short(short actual, short expected, const char * msg, ...);
+/* This function compares the two long <<actual>> and <<expected>>
+ * 	If they are equal, the function returns 0
+ * 	If they are not equal, the formatted message <<msh, ...>>
+ * 		is printed and -1 is returned
+ */
 int assert_equals_long(long actual, long expected, const char * msg, ...);
+/* This function compares the two long long <<actual>> and <<expected>>
+ * 	If they are equal, the function returns 0
+ * 	If they are not equal, the formatted message <<msh, ...>>
+ * 		is printed and -1 is returned
+ */
 int assert_equals_long_long(long long actual, long long expected, const char * msg, ...);
 
+/* This function compares the two float <<actual>> and <<expected>>
+ * with an accuracy of <<epsilon>>
+ * 	If they are equal, the function returns 0
+ * 	If they are not equal, the formatted message <<msh, ...>>
+ * 		is printed and -1 is returned
+ */
 int assert_equals_float(float actual, float expected, float epsilon, const char * msg, ...);
+/* This function compares the two double <<actual>> and <<expected>>
+ * with an accuracy of <<epsilon>>
+ * 	If they are equal, the function returns 0
+ * 	If they are not equal, the formatted message <<msh, ...>>
+ * 		is printed and -1 is returned
+ */
 int assert_equals_double(double actual, double expected, double epsilon, const char * msg, ...);
 
+/* This function compares the two strings <<actual>> and <<expected>>
+ * with an accuracy of <<epsilon>>
+ * 	If they are equal, the function returns 0
+ * 	If they are not equal, the formatted message <<msh, ...>>
+ * 		is printed and -1 is returned
+ */
 int assert_equals_str(char * actual, char * expected, const char * msg, ...);
+/* This function compares byte by byte the memory pointed by <<actual>> and <<expected>>
+ * up to a total number of <<n_bytes>> bytes
+ * 	If all of the <<n_bytes>> bytes are equal, the function returns 0
+ * 	If not all of the <<n_bytes>> are not equal, the formatted message <<msh, ...>>
+ * 		is printed and -1 is returned
+ */
 int assert_equals_ptr(void * actual, void * expected, size_t n_bytes, const char * msg, ...);
+/* This function compares two objects passed as raw memory <<actual>> and <<expected>>,
+ * using a function <<comp(void *, void *)>> passed as parameter.
+ * The function <<comp(void *, void *)>> should return 0 if both of the objects passed as parameters
+ * are equal, and other value otherwise
+ * 	If the arguments are equal, the function returns 0
+ * 	If they are not equal, the formatted message <<msh, ...>>
+ * 		is printed and -1 is returned
+ */
 int assert_equals_obj(void * actual, void * expected, int (* comp) (void *, void *), const char * msg, ...);
+
+/*
+ * From here below all the functions are the negated logic versions of the above
+ */
 
 int assert_not_equals_int(int actual, int expected, const char * msg, ...);
 int assert_not_equals_char(char actual, char expected, const char * msg, ...);
